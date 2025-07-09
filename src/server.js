7a24032edf7e03f57ce7086a792a3caef59c53ac
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2025-07-10 00:45:58 +0530

    Add dotenv for environment variable management and update server port configuration

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import router from './routes/emailRoutes.js';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use('/api', router);
 
+
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>🚀 server is running !! </h1>`
+    );
+});
+
 app.listen(3000, () => {
-  console.log('🚀 Server running on http://localhost:3000');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
